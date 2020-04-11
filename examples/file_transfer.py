5b96b1d350fd949656e640b6c1bbb6b81e0b5427
iamkroot <kroot.patel@gmail.com>
2020-04-11 12:58:25 +0530

    Add file_transfer example

---
+++ examples/file_transfer.py
@@ -0,0 +1,101 @@
+import argparse
+import ipaddress
+import logging
+import time
+from pathlib import Path
+from YARU import YARUSocket
+
+
+NAME_MARKER = b":name:"
+END_MARKER = b":end:"
+
+
+def receiver(address: ipaddress.IPv4Address, port: int, directory: Path, **kwargs):
+    sock = YARUSocket()
+    sock.bind((str(address), port))
+    logging.info(f"Receiver bound to {address=}, {port=}")
+    directory.mkdir(exist_ok=True)
+
+    file_name = None
+    file = None
+
+    while True:
+        data = sock.read()
+        if not data:
+            if not file_name:
+                time.sleep(1)  # Idle mode
+            else:
+                time.sleep(0.1)  # active connection
+            continue
+        if data.startswith(NAME_MARKER):  # first packet has filename
+            file_name = data[len(NAME_MARKER) :].decode()
+            file = open(directory / file_name, "wb")
+            size = 0
+            logging.info(f"Receiving {file_name}")
+        elif data == END_MARKER:  # finished sending data
+            file.close()
+            break
+        else:  # file contents received, store to file
+            size += len(data)
+            file.write(data)
+    logging.info(f"Completed receiving {file_name}, {size} bytes")
+
+
+def sender(address: ipaddress.IPv4Address, port: int, file: Path, **kwargs):
+    sock = YARUSocket()
+    sock.connect((str(address), port))
+
+    logging.info(f"Sending {file.name}")
+    sock.write(NAME_MARKER + str(file.name).encode("utf-8"))
+    file_data = file.read_bytes()
+    total_size = len(file_data)
+    pkt_size = YARUSocket.MAX_DATA_SIZE
+    offset = 0
+
+    while offset < total_size:
+        sock.write(file_data[offset : offset + pkt_size])
+        offset += pkt_size
+    sock.write(END_MARKER)
+    logging.info(f"Completed sending {file.name}, {total_size} bytes")
+
+
+if __name__ == "__main__":
+    log_choices = {}
+    for level in range(logging.CRITICAL + 1):
+        name = logging.getLevelName(level)
+        if name.startswith("Level"):
+            continue
+        log_choices[name] = level
+
+    parser = argparse.ArgumentParser(
+        description="Send and Receive files over YARU.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-a",
+        "--address",
+        type=ipaddress.IPv4Address,
+        default="127.0.0.1",
+        help="Which IP address to bind/connect to",
+    )
+    parser.add_argument(
+        "-p", "--port", type=int, default=1060, help="Receiver port",
+    )
+    parser.add_argument("--loglevel", choices=log_choices, default="DEBUG")
+    subparser = parser.add_subparsers(help="Which role to play")
+    parser_sender = subparser.add_parser("sender")
+    parser_sender.add_argument("file", type=Path, help="The file to send")
+    parser_sender.set_defaults(func=sender)
+    parser_receiver = subparser.add_parser("receiver")
+    parser_receiver.add_argument(
+        "-d",
+        "--directory",
+        type=Path,
+        default="Received",
+        help="Where to store received file",
+    )
+    parser_receiver.set_defaults(func=receiver)
+    args = parser.parse_args()
+    if not hasattr(args, "func"):
+        raise argparse.ArgumentError(None, "Invalid role chosen")
+    args.func(**vars(args))
